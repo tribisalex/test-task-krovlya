ae219f87b8eacf89507690fe1cb65e3f295d9890
Tribisalex <tribisalexandr@yandex.ru>
2022-06-24 12:56:22 +0300

    feat: add adaptiv from width<992

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -2,15 +2,26 @@ const modal = document.querySelector("#modal");
 const modalOverlay = document.querySelector("#modal-overlay");
 const closeButton = document.querySelector("#close-button");
 const openButton = document.querySelector("#open-button");
+const phone = document.getElementById("phone");
+const check = document.getElementById("check");
+const buttonSubmit = document.getElementById("submit-button");
 
 closeButton.addEventListener("click", function () {
   modal.classList.toggle("closed");
   modalOverlay.classList.toggle("closed");
 });
 
-
-
 openButton.addEventListener("click", function () {
   modal.classList.toggle("closed");
   modalOverlay.classList.toggle("closed");
 });
+
+buttonSubmit.addEventListener("click", function () {
+  if (check.checked === true && phone.value) {
+    check.checked = false;
+    phone.value = ''
+    modal.classList.toggle("closed");
+    modalOverlay.classList.toggle("closed");
+    alert('Спасибо, Ваша заявка отправлена');
+  }
+})
