695c78b110f3a86a3bcffba5971066505e717154
Tribisalex <tribisalexandr@yandex.ru>
2022-06-25 01:27:47 +0300

    feat: add drop-down block

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,11 +1,13 @@
 const modal = document.querySelector("#modal");
 const modalOverlay = document.querySelector("#modal-overlay");
 const closeButton = document.querySelector("#close-button");
+const closeAdvantages = document.querySelector(".close-advantages");
 const openButton = document.querySelector("#open-button");
 const openButton1 = document.querySelector("#open-button1");
 const phone = document.getElementById("phone");
 const check = document.getElementById("check");
 const buttonSubmit = document.getElementById("submit-button");
+const slide = document.querySelector(".slide");
 
 closeButton.addEventListener("click", function () {
   modal.classList.toggle("closed");
@@ -31,4 +33,12 @@ buttonSubmit.addEventListener("click", function () {
     modalOverlay.classList.toggle("closed");
     alert('Спасибо, Ваша заявка отправлена');
   }
+})
+
+
+closeAdvantages.addEventListener("click", function() {
+  modalOverlay.classList.toggle("closed");
+  slide.classList.toggle("slide-show");
+  closeAdvantages.classList.toggle("close-button-show");
+
 })
